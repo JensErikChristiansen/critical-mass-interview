bf4b7793a88f5cadaae00bc7cdc17d0935e350fe
Jens Christiansen <undergroundviking@gmail.com>
2024-05-14 12:20:34 -0700

    disable underline transition while resizing

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-main();
+document.addEventListener("DOMContentLoaded", () => {
+  main();
+});
 
 const ACTIVE_CLASS = "active";
 
@@ -9,22 +11,33 @@ async function main() {
   $cities.innerHTML = createLinks(cities);
   // store in-memory so we don't have to keep querying $cities
   const links = [...$cities.children].map((city) => city.children[0]);
+  let $activeLink = null;
 
   $cities.addEventListener("click", (e) => {
-    let link;
-
     if (e.target.tagName === "A") {
-      link = e.target;
+      $activeLink = e.target;
     } else if (e.target.tagName === "LI") {
-      link = e.target.children[0];
-    }
+      $activeLink = e.target.children[0];
+    } else return;
 
     links.forEach((link) => link.classList.remove(ACTIVE_CLASS));
-    link.classList.add(ACTIVE_CLASS);
-    $nav.style.setProperty("--underline-left", link.offsetLeft + "px");
-    $nav.style.setProperty("--underline-width", link.offsetWidth + "px");
+    $activeLink.classList.add(ACTIVE_CLASS);
+    $nav.style.setProperty("--underline-left", $activeLink.offsetLeft + "px");
+    $nav.style.setProperty("--underline-width", $activeLink.offsetWidth + "px");
 
     const city = e.target.dataset.city; // TODO: fetch time
+  });
+
+  window.addEventListener("resize", () => {
+    $nav.style.setProperty("--underline-left", $activeLink.offsetLeft + "px");
+    $nav.style.setProperty("--underline-width", $activeLink.offsetWidth + "px");
+
+    // temporarily disable the underline animation while resizing
+    $nav.style.setProperty("--underline-transition", 0);
+
+    createDebounce()(() => {
+      $nav.style.setProperty("--underline-transition", "all 300ms ease-out");
+    });
   });
 }
 
@@ -44,4 +57,16 @@ function createLinks(cities) {
   }, "");
 
   return html;
+}
+
+function createDebounce() {
+  let timer;
+
+  return (fn, delay = 200) => {
+    if (timer) {
+      clearTimeout(timer);
+    }
+
+    timer = setTimeout(fn, delay);
+  };
 }
