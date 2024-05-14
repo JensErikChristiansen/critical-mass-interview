06650edea13dfdea82dba3360b12b5a982637405
Jens Christiansen <undergroundviking@gmail.com>
2024-05-14 13:35:44 -0700

    get time

--- index.mjs
+++ index.mjs
@@ -1,37 +1,41 @@
+import timezones, { TIMEZONE_BASE_URL } from "./timezones.mjs";
+
+const ACTIVE_CLASS = "active";
+
 document.addEventListener("DOMContentLoaded", () => {
   main();
 });
 
-const ACTIVE_CLASS = "active";
-
 async function main() {
   const cities = await fetchCities();
-  const $cities = document.getElementById("cities");
   const $nav = document.getElementById("cities-nav");
+  const $time = document.getElementById("time");
+  const $date = document.getElementById("date");
+  const $cities = document.getElementById("cities");
   $cities.innerHTML = createLinks(cities);
   // store in-memory so we don't have to keep querying $cities
-  const links = [...$cities.children].map((city) => city.children[0]);
+  const $links = [...$cities.children].map((city) => city.children[0]);
   let $activeLink = null;
 
-  $cities.addEventListener("click", (e) => {
+  $cities.addEventListener("click", async (e) => {
     if (e.target.tagName === "A") {
       $activeLink = e.target;
     } else if (e.target.tagName === "LI") {
       $activeLink = e.target.children[0];
     } else return;
 
-    links.forEach((link) => link.classList.remove(ACTIVE_CLASS));
+    $links.forEach((link) => link.classList.remove(ACTIVE_CLASS));
     $activeLink.classList.add(ACTIVE_CLASS);
     $nav.style.setProperty("--underline-left", $activeLink.offsetLeft + "px");
     $nav.style.setProperty("--underline-width", $activeLink.offsetWidth + "px");
 
-    const city = e.target.dataset.city; // TODO: fetch time
+    const city = $activeLink.dataset.city;
+    await updateDateTime(city, $activeLink.ariaLabel);
   });
 
   window.addEventListener("resize", () => {
     $nav.style.setProperty("--underline-left", $activeLink.offsetLeft + "px");
     $nav.style.setProperty("--underline-width", $activeLink.offsetWidth + "px");
-
     // temporarily disable the underline animation while resizing
     $nav.style.setProperty("--underline-transition", 0);
 
@@ -39,6 +43,34 @@ async function main() {
       $nav.style.setProperty("--underline-transition", "all 300ms ease-out");
     });
   });
+
+  const $city = document.getElementById("city");
+  updateDateTime();
+
+  async function updateDateTime(city, label) {
+    const timezone =
+      timezones[city] || Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+    const res = await fetch(TIMEZONE_BASE_URL + timezone);
+    const json = await res.json();
+    // remove the offset so we get THEIR local time
+    const dateTimeString = json.datetime.replace(/\+.*$/, "");
+    const dateTime = new Date(dateTimeString);
+
+    $city.textContent = label || "Here";
+
+    $time.textContent = dateTime.toLocaleTimeString(undefined, {
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+
+    $date.textContent = new Date(dateTimeString).toLocaleDateString(undefined, {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+  }
 }
 
 async function fetchCities() {
@@ -52,7 +84,7 @@ function createLinks(cities) {
   const html = cities.reduce((acc, c) => {
     return `${acc}
     <li class="city">
-      <a data-city="${c.section}" href="#">${c.label}</a>
+      <a data-city="${c.section}" href="#" aria-label="${c.label}">${c.label}</a>
     </li>`;
   }, "");
 
---
+++ timezones.mjs
@@ -0,0 +1,11 @@
+export default {
+  cupertino: "America/Los_Angeles",
+  "new-york-city": "America/New_York",
+  london: "Europe/London",
+  amsterdam: "Europe/Amsterdam",
+  tokyo: "Asia/Tokyo",
+  "hong-kong": "Asia/Hong_Kong",
+  sydney: "Australia/Sydney",
+};
+
+export const TIMEZONE_BASE_URL = "https://worldtimeapi.org/api/timezone/";
